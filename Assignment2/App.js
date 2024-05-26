710566b801547447e9ad1fcd270c90ec5ac10754
Caleb Mawusi <cemawusi@st.ug.edu.gh>
2024-05-26 15:49:27 +0000

    Changed background colour

--- Assignment2/App.js
+++ Assignment2/App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff',
+    backgroundColor: '#2E3135',
     alignItems: 'center',
     justifyContent: 'center',
   },
