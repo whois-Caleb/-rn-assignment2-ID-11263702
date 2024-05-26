891196a73fb2e6b8b4e8f560e83ae34c63f54db1
Caleb Mawusi <cemawusi@st.ug.edu.gh>
2024-05-26 15:53:47 +0000

    Edited text component

--- Assignment2/App.js
+++ Assignment2/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>My name is Caleb.</Text>
       <StatusBar style="auto" />
     </View>
   );
