d39a1197bbea0ed582f25d366bcb97b5e913097a
Caleb Mawusi <cemawusi@st.ug.edu.gh>
2024-05-26 15:52:05 +0000

    Changed text colour
    Improved text visibility on new background

--- Assignment2/App.js
+++ Assignment2/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#2E3135',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
   },
 });
